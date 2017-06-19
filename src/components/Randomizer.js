044a783292b966d88e496bd56d349d0a561c6e7c
Nima Seyf <nimaseyf@gmail.com>
2017-06-19 09:49:21 +0100

    added react-router-dom

---
+++ src/components/Randomizer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Randomizer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { '' }
+  }
+
+render() {
+  return (
+    <div> OK! </div>
+  )
+}
+
+}
