9d8c76cd2bae32d1b8f853d77af4e9a4d5612df5
Nima Seyf <nimaseyf@gmail.com>
2017-06-20 19:27:16 +0100

    fixed css

--- src/components/ShowLocations.js
+++ src/components/ShowLocations.js
@@ -9,7 +9,7 @@ renderLocations(location){
 const { name, backgroundImage } = location;
   return (
     <Link to={`/${location.name}`} key={location.name}>
-    <div key={name} className="location-cell col-lg-4 col-xs-12 col-md-6" style={{ cursor: "pointer", border: "5px solid white", textAlign: "center", height: "300px"}}>
+    <div key={name} className="location-cell col-lg-4 col-xs-12 col-md-6" style={{ cursor: "pointer", border: "2px solid white", textAlign: "center", height: "300px"}}>
       <h2 className="location-title">{name.toUpperCase()}</h2>
     </div>
   </Link>
