b7c163a9768e7b14c5573f8b4ceee74e45752066
Nima Seyf <nimaseyf@gmail.com>
2018-01-12 18:34:18 +0000

    commented & cleaned code

--- src/components/Randomizer.js
+++ src/components/Randomizer.js
@@ -2,42 +2,64 @@ import React, { Component } from 'react';
 import { Locations } from '../data/locationinfo';
 import _ from 'lodash';
 
+
 export default class Randomizer extends Component {
 constructor(props) {
   super(props);
 
   const location = this.props.location.pathname.slice(1);
+
+  // We search the data for the object that matches the location.
   const data = _.find(Locations, { name: location });
 
-// Need Icons fa-crosshairs(weapon) + fa-user-secret(disguise);
+// State is initialised with the data object , the targets from the location specified as well as an active boolean
+// that acts as a trigger for alternate CSS.
+
   this.state = {
     data,
     targets: data.targets,
     active: false
   }
 }
-
+// Ensures window scrolls to top when loading the randomizer component.
 componentDidMount() {
   window.scrollTo(0, 0);
 }
+
+// Function to generate the randomised win conditions. Each press of the Randomize button
+// generates a unique combination of disguises and weapons for the game.
 
 handleClick(e) {
   e.preventDefault();
 window.scrollTo(200,200);
 
+// We create a copy of the state. This allows us
+// to make several changes and then setState
+// with the single new object in one go.
 const newState = Object.assign({}, this.state);
 
+// We grab the targets array from the data object
 newState.data.targets = newState.data.targets.slice();
 
+// The targets sliced from the data object are then mapped over
 newState.data.targets.map(target =>
+  // Each target has killedwith and whileWearing properties
+  // that are initially blank.
+  // _.sample is used to return a random element from the weapons
+  // and disguises arrays.
   target.killedWith = `${_.sample(newState.data.weapons)}`
 );
 
 newState.data.targets.map(target =>
+
 target.whileWearing = `${_.sample(newState.data.disguises)}`);
+
+// Finally active is set to true, triggering a visual
+// change in the application.
 
 newState.active = true;
 
+// Last but not least we replace the state with the newState object.
 this.setState(newState);
 
 }
@@ -47,7 +69,10 @@ renderTargets(targets) {
 const taskStyle = { fontWeight: "600", color: "#151515"};
 const taskDescriptorStyle = {fontWeight: "100", color: "#151515"};
 
-// #F5F5F5, #151515
+// renderTargets maps over each target in targets array to return a card block
+// As initial values for killedWith and whileWearing are blank,
+// Ternary operators are used to ensure the app does not try to
+// render data that does not initially exist.
 
  return targets.map(target =>
    (
@@ -66,13 +91,21 @@ const taskDescriptorStyle = {fontWeight: "100", color: "#151515"};
   ) );
 }
 
+
 render() {
 
   return (
   <div className="container">
     <div className="row">
     <button style={{
-      borderRadius: "0",  fontSize: "25px", fontWeight: "600", background: "#F5F5F5", color: "#151515", marginBottom: "25px", height: "75px", textAlign: "center"}} className="btn btn-primary" onClick={this.handleClick.bind(this)}>Randomize</button>
+      borderRadius: "0",
+      fontSize: "25px",
+      fontWeight: "600",
+      background: "#F5F5F5",
+      color: "#151515",
+      marginBottom: "25px",
+      height: "75px",
+      textAlign: "center"}} className="btn btn-primary" onClick={this.handleClick.bind(this)}>Randomize</button>
     </div>
     <div className="row">
     <div className="card-group">
--- src/components/ShowLocations.js
+++ src/components/ShowLocations.js
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 
 export default class ShowLocations extends Component {
 
+// For each element in the location array the app
+// returns a link to the Randomizer page and a div with the
+// location title on it the user can click on.
 renderLocations(location){
-
+  // Grabbing name from the location object using ES6 destructuring.
 const { name } = location;
   return (
     <Link to={`/${location.name}`} key={location.name}>
